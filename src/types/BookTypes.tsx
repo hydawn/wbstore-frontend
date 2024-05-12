90c3edaba7e5b141cd3fd57612e7c56e80ecef30
Hanyu Duan <HanyuDuan@outlook.com>
2024-05-12 15:09:10 +0800

    add unified book types

---
+++ src/types/BookTypes.tsx
@@ -0,0 +1,10 @@
+export interface BookInfo {
+  id: string,
+  name: string,
+  text_description: string,
+  image_description: string,
+  price: number,
+  online_date: string,
+  stock_inventory: number,
+  added_by_user: number
+}
